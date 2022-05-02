abc878d73ab26dfd3b0565b6c006cc77f6e88902
“haoping2046” <pinghao2017@gmail.com>
2022-05-02 10:59:09 -0700

    templateMethod

---
+++ src/templateMethod/AbstractClass.java
@@ -0,0 +1,6 @@
+package templateMethod;
+
+public abstract class AbstractClass {
+    abstract public void primitiveOperation1();
+    abstract public void primitiveOperation2();
+}
---
+++ src/templateMethod/ConcreteClass.java
@@ -0,0 +1,17 @@
+package templateMethod;
+
+public class ConcreteClass extends AbstractClass {
+
+	@Override
+	public void primitiveOperation1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void primitiveOperation2() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
